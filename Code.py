81f872e2e6245073ee7c51b32d24a17f08f9eadd
BSienkiewicz48 <150445140+BSienkiewicz48@users.noreply.github.com>
2025-05-22 12:13:13 +0200

    dodane wyswietlanie plota

--- Code.py
+++ Code.py
@@ -1,6 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import streamlit as st
 
 df = pd.read_parquet(r'raw_data.parquet')
 
@@ -12,3 +13,5 @@ cols = [
 corr_matrix = df[cols].corr()
 sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap="coolwarm")
 plt.title("Correlation Matrix for Selected Features")
+
+st.pyplot(plt)
