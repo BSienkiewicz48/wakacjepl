ebea4977b994959eff213f02073112b8ef963c82
BSienkiewicz48 <150445140+BSienkiewicz48@users.noreply.github.com>
2025-05-22 12:04:13 +0200

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,14 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_parquet(r'dataset.parquet')
+
+cols = [
+    'popularity', 'duration_ms', 'danceability', 'energy', 'loudness',
+    'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo'
+]
+
+corr_matrix = df[cols].corr()
+sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap="coolwarm")
+plt.title("Correlation Matrix for Selected Features")
